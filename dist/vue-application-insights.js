3f56d81080315531b31595055d121887b3bc1cf1
vikjon03 <vikjon03@A023920>
2019-04-03 13:18:10 +0200

    Added support for letting the AppInsights instance to be initialized outside of Vue

--- dist/vue-application-insights.js
+++ dist/vue-application-insights.js
@@ -7,7 +7,7 @@ Object.defineProperty(exports, "__esModule", {
 var _applicationinsightsJs = require('applicationinsights-js');
 
 /**
- * Install function passed to Vue.use() show documentation in vue.js website.
+ * Install function passed to Vue.use() show documentation on vue.js website.
  *
  * @param Vue
  * @param options
@@ -16,34 +16,57 @@ function install(Vue, options) {
   var id = options.id;
 
 
-  Vue.appInsights = _applicationinsightsJs.AppInsights;
-  Vue.appInsights.downloadAndSetup({ instrumentationKey: id });
+  if (options.appInsights) {
+    Vue.appInsights = options.appInsights;
+  } else {
+    Vue.appInsights = _applicationinsightsJs.AppInsights;
+    Vue.appInsights.downloadAndSetup({ instrumentationKey: id });
+  }
 
   var router = options.router;
 
   // Watch route event if router option is defined.
   if (router) {
 
-    var basePath = options.basePath || '(Application Root)';
-
-    var pathFormatter = function pathFormatter(path) {
-      return basePath + '/' + path.substr(1);
-    };
-
-    router.beforeEach(function (route, from, next) {
-      Vue.appInsights.startTrackPage(pathFormatter(route.fullPath));
-      next();
-    });
-
-    router.afterEach(function (route) {
-      Vue.appInsights.stopTrackPage(pathFormatter(route.fullPath), route.fullPath);
-    });
+    if (options.trackInitialPageView !== false) {
+      setupPageTracking(options, Vue);
+    } else {
+      router.onReady(function () {
+        return setupPageTracking(options, Vue);
+      });
+    }
   }
 
   Object.defineProperty(Vue.prototype, '$appInsights', {
     get: function get() {
       return Vue.appInsights;
     }
+  });
+}
+
+/**
+ * Track route changes as page views with AppInsights
+ * @param options 
+ */
+function setupPageTracking(options, Vue) {
+
+  var router = options.router;
+
+  var basePath = options.basePath || '(Application Root)';
+
+  var pathFormatter = function pathFormatter(path) {
+    return basePath + '/' + path.substr(1);
+  };
+
+  router.beforeEach(function (route, from, next) {
+    var path = pathFormatter(route.fullPath);
+    Vue.appInsights.startTrackPage(path);
+    next();
+  });
+
+  router.afterEach(function (route) {
+    var path = pathFormatter(route.fullPath);
+    Vue.appInsights.stopTrackPage(path, route.fullPath);
   });
 }
 
--- src/index.js
+++ src/index.js
@@ -10,28 +10,24 @@ import { AppInsights } from "applicationinsights-js"
 function install (Vue, options) {
 
   const { id } = options
-
-  Vue.appInsights = AppInsights
-  Vue.appInsights.downloadAndSetup({ instrumentationKey: id })
+  
+  if (options.appInsights) {
+    Vue.appInsights = options.appInsights
+  } else {
+    Vue.appInsights = AppInsights
+    Vue.appInsights.downloadAndSetup({ instrumentationKey: id }) 
+  }
 
   const router = options.router
 
   // Watch route event if router option is defined.
   if (router) {
 
-    const basePath = options.basePath || '(Application Root)'
-
-    const pathFormatter = path =>
-    basePath + '/' + path.substr(1)
-
-    router.beforeEach( (route, from, next) => {
-      Vue.appInsights.startTrackPage(pathFormatter(route.fullPath))
-      next()
-    })
-
-    router.afterEach( route => {
-      Vue.appInsights.stopTrackPage(pathFormatter(route.fullPath), route.fullPath)
-    })
+    if (options.trackInitialPageView !== false) {
+      setupPageTracking(options, Vue);
+    } else {
+      router.onReady(() => setupPageTracking(options, Vue))
+    }
 
   }
 
@@ -39,6 +35,31 @@ function install (Vue, options) {
     get: () => Vue.appInsights
   })
 
+}
+
+/**
+ * Track route changes as page views with AppInsights
+ * @param options 
+ */
+function setupPageTracking(options, Vue) {
+
+  const router = options.router
+
+  const basePath = options.basePath || '(Application Root)'
+
+  const pathFormatter = path =>
+  basePath + '/' + path.substr(1)
+
+  router.beforeEach( (route, from, next) => {
+    const path = pathFormatter(route.fullPath);
+    Vue.appInsights.startTrackPage(path)
+    next()
+  })
+
+  router.afterEach( route => {
+    const path = pathFormatter(route.fullPath);
+    Vue.appInsights.stopTrackPage(path, route.fullPath)
+  })
 }
 
 // auto install for navigator
